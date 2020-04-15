eef2b173ed8a2a6035ee5eec695842cfe55adc20
reo <bre-bbb_rin-going.3596@hotmail.co.jp>
2020-04-15 14:02:45 +0000

    mynews

---
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ProfileController extends Controller
+{
+    public function add()
+    {
+      returnvew('admin.profile.create');
+    }
+    
+    public function create()
+    {
+      return redirect('admin/profile/create');
+    }
+    
+    public function edit()
+    {
+      return view('admin.profile.edit');
+    }
+    
+    public function update()
+    {
+      return redirect('admin/profile/edit');
+    }
+}
